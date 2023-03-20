c0bfe3dfc47805617f9a9210caa6fa4ece967d14
Matheus Zanela <matheusdeveloper001@gmail.com>
2023-03-20 17:23:09 -0300

    last commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { AppProps } from "next/app";
 import GlobalStyles from "@/styles/global";
 import { ThemeProvider } from "styled-components";
@@ -5,14 +6,21 @@ import theme from "@/styles/theme";
 import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const client = new ApolloClient({
+    uri: "http://localhost:1337/graphql",
+    cache: new InMemoryCache()
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 
